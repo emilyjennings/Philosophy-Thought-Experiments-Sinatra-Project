ff73312489f3c519eda8a2524a9a081ba9c09e33
github username <github email address>
2018-11-13 19:30:48 -0500

    played around with the order of contoller methods, fixed login issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,63 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    @all_users = User.all
-    @user = current_user
-    erb :"/users/index.html"
-  end
-
-  # GET: /users/new
   get "/users/new" do
     erb :"/users/new.html"
   end
 
-  # POST: /users
+  # GET: /users
+  get "/users" do
+    if logged_in?
+      @all_users = User.all
+      @user = current_user
+      erb :"/users/index.html"
+    else
+      redirect "/users/new"
+    end
+  end
+
+  # # POST: /users
+  # post "/users" do
+  #   @user = User.find_by(username: params[:username])
+  #   if @user
+  #     @user = User.create(username: params[:username])
+  #     @user.save
+  #     session[:user_id] = @user.id
+  #     redirect "/users"
+  #   else
+  #     redirect "/users"
+  #   end
+  # end
+
   post "/users" do
     @user = User.find_by(username: params[:username])
-    if @user
+    if !@user
       @user = User.create(username: params[:username])
       @user.save
       session[:user_id] = @user.id
       redirect "/users"
     else
+      session[:user_id] = @user.id
       redirect "/users"
     end
   end
 
-  # GET: /users/5 - shows a profile page with links to the stories they uploaded
-  get "/users/:id" do
-    erb :"/users/show.html"
+  get "/users/login" do
+    erb :"users/login.html"
   end
+
+
+
+
+
+
+  # GET: /users/5 - shows a profile page with links to the stories they uploaded
+  # get "/users/:id" do
+  #   erb :"/users/show.html"
+  # end
 
   get '/users/logout' do
       session.clear
-      redirect "/"
+      erb  :"/users/logout"
   end
 
   # # GET: /users/5/edit

1a17db40e254fe64b63bc7cea32e9f12f3716cd2
github username <github email address>
2018-11-17 15:33:04 -0500

    user show page with links working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,31 +17,50 @@ class UsersController < ApplicationController
   end
 
   # creates a new user if fields are filled in
+  # validates the user is real and logs in to a new session
+
   post "/users" do
     @user = User.find_by(username: params[:username])
     if !@user && !params[:username].empty?
       @user = User.create(username: params[:username])
       @user.save
+      # @user = current_user
       session[:user_id] = @user.id
       redirect "/users"
     else
-      session[:user_id] = @user.id
-      redirect "/users"
+      redirect "/start"
     end
   end
 
+  #just takes to a login page for a repeat user
   get "/users/login" do
     erb :"users/login.html"
   end
 
-  #GET: /users/5 - shows a profile page with links to the stories they uploaded
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if !@user || params[:username].empty?
+      redirect "/start"
+    elsif @user
+      session[:user_id] = @user.id
+      redirect "/users"
+    else
+      redirect "/start"
+    end
+  end
+
+  #logs out and takes to a page to start over again
+  get '/users/logout' do
+      session.clear
+      erb :"/users/logout"
+  end
+
+  #shows a profile page with links to the stories they uploaded
   get "/users/:id" do
+    @user = User.find_by(id: current_user.id)
     erb :"/users/show.html"
   end
 
-  get '/users/logout' do
-      session.clear
-      erb  :"/users/logout"
-  end
+
 
 end
